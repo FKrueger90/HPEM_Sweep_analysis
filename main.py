def4e85b6c74b20fc07caa541cf15b682c4648e9
fkrueger <florian.krueger@protonmail.com>
2021-09-28 10:45:20 -0400

    working version 9/28

--- main.py
+++ main.py
@@ -11,18 +11,17 @@ cases = []
 newCase = Case()
 cases.append(newCase)
 
-print(cases[0].powerICP)
-
 # root directory
-dir_root = os.path.abspath("D:\\UIGEL5_D_Florian\Voltage_Waveform_Tailoring\HPEM\ArCF4O2\DarkSpaceGeometry\ConstVoltage_200_1000")
+dir_root = os.path.abspath("C:\\Users\\flori\\Desktop\\temp2\\ConstVoltage_200_1000")
 # dir_root = "D:\\UIGEL5_D_Florian\Voltage_Waveform_Tailoring\HPEM\ArCF4O2\DarkSpaceGeometry\ConstVoltage_200_1000"
 dirs_in_root = os.listdir(dir_root)
-print(dirs_in_root)
 
-# print header
+# find source and case folders
+# --------------------------------------------------------------
 
-print("type", 10*" ", "name")
-print(50*"-")
+# print file summary header
+print("type" + 12*" " + "name" + "\n" + 50*"-")
+
 for dir_in_root in dirs_in_root:
 
     dir_sub = os.path.join(dir_root, dir_in_root)
@@ -35,15 +34,12 @@ for dir_in_root in dirs_in_root:
     if os.path.isdir(dir_sub):
         string_dir_type = "{0:<17}".format("directory")
 
-        # if directory, find out if source or case directory
-        # ---------------------------------------------------
+        # if directory, find out if source or case directory or neither
+        # -------------------------------------------------------------------
         paths_in_sub = os.listdir(dir_sub)
         for path_in_sub in paths_in_sub:
-            #print(path_in_sub)
+            # print(path_in_sub)
             if os.path.isfile(os.path.join(dir_sub, path_in_sub)):
-                #print("here")
-                #print(path_in_sub)
-
                 if "icp.f" in path_in_sub.lower() or "icp.for" in path_in_sub.lower():
                     string_dir_type = "{0:<17}".format("source directory")
 
